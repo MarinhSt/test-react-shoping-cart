8b0d963c9925d9aa80a731ae37a6d95c3e0e843d
MarinhSt <st_marinh@protonmail.com>
2024-07-16 20:40:08 -0500

    Added some quick inline styles for the list of products.

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,27 @@ function App() {
             </header>
             <main>
                 <ul
-                    
+                    style={{
+                        listStyle: 'none',
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent: 'center',
+                        gap: '20px 10px',
+                        
+                    }}
                 >
                     {products.map(product => (
                         <li
-                            
                             key={product.id}
+                            style={{
+                                width: '200px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                boxShadow:'#6596f1a3 0px 0px 12px 0px'
+                            }}
                         >
-                            <img
-                                src={product.thumbnail}
-                            />
+                            <img src={product.thumbnail} />
                             <h3>{product.title}</h3>
                             <p>{product.description}</p>
                             <span>

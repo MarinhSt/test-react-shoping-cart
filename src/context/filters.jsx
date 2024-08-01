5d99012d935c2e0e7b9216718086fe0461094c90
MarinhSt <st_marinh@protonmail.com>
2024-07-31 22:54:46 -0500

    Created the FilterContext and FilterProvider

---
+++ src/context/filters.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react'
+
+export const FilterContext = createContext()
+
+export function FilterProvider({ children }) {
+    const [filterBy, setFilterBy] = useState({
+        category: 'All',
+        from: 0,
+        to: 9999.99,
+    })
+    return (
+        <FilterContext.Provider value={(filterBy, setFilterBy)}>
+            {children}
+        </FilterContext.Provider>
+    )
+}

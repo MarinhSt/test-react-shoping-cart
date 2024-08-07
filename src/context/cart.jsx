7787af112b7ddb9cead720bbb2ee5e1cd55bbcf2
MarinhSt <st_marinh@protonmail.com>
2024-08-07 12:02:55 -0500

    Cleaned the CartContext data.

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -3,32 +3,7 @@ import { createContext, useState } from 'react'
 export const CartContext = createContext()
 
 export function CartProvider({ children }) {
-    const [addedToCart, setAddedToCart] = useState([
-        {
-            id: 1,
-            price: 9.99,
-            quantity: 1,
-            thumbnail:
-                'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png',
-            title: 'Essence Mascara Lash Princess',
-        },
-        {
-            id: 2,
-            price: 19.99,
-            quantity: 1,
-            thumbnail:
-                'https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png',
-            title: 'Eyeshadow Palette with Mirror',
-        },
-        {
-            id: 3,
-            price: 14.99,
-            quantity: 1,
-            thumbnail:
-                'https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/thumbnail.png',
-            title: 'Powder Canister',
-        },
-    ])
+    const [addedToCart, setAddedToCart] = useState([])
 
     return (
         <CartContext.Provider value={{ addedToCart, setAddedToCart }}>

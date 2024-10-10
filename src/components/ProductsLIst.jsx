57066e007a7f29ff3c5ecffbe7e98687cd5ab0ff
MarinhSt <st_marinh@protonmail.com>
2024-10-10 01:49:10 -0500

    Changed the AddToCart button when the product has been successfully added to the cart.

--- src/components/ProductsLIst.jsx
+++ src/components/ProductsLIst.jsx
@@ -7,13 +7,17 @@ export function ProductsLIst() {
     const filteredProducts = filterProducts(products)
     const { addedToCart, setAddedToCart } = useContext(CartContext)
     const addToCart = product => {
-        const { id, thumbnail, title, price } = product
-        const newProduct = { id, thumbnail, title, price, quantity: 1 }
+        const { id, thumbnail, title, price, stock } = product
+        const newProduct = { id, thumbnail, title, price, stock, quantity: 1 }
         if (!addedToCart) {
             setAddedToCart([...newProduct])
         } else {
             setAddedToCart([...addedToCart, newProduct])
         }
+    }
+
+    const checkProductInCart = id => {
+        return addedToCart.some(product => product.id == id)
     }
     return (
         <ul
@@ -25,35 +29,40 @@ export function ProductsLIst() {
                 gap: '20px 10px',
             }}
         >
-            {filteredProducts.map(product => (
-                <li
-                    key={product.id}
-                    style={{
-                        width: '200px',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        boxShadow: '#6596f1a3 0px 0px 12px 0px',
-                    }}
-                >
-                    <img src={product.thumbnail} />
-                    <h3>{product.title}</h3>
-                    <span>
-                        <b>PRICE: $</b>
-                        {product.price}
-                    </span>
-                    <button
+            {filteredProducts.map(product => {
+                const productInCart = checkProductInCart(product.id)
+                console.log(productInCart)
+                return (
+                    <li
+                        key={product.id}
                         style={{
-                            width: 'fit-content',
-                            alignSelf: 'center',
-                            margin: '10px',
+                            width: '200px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            boxShadow: '#6596f1a3 0px 0px 12px 0px',
                         }}
-                        onClick={() => addToCart(product)}
                     >
-                        add to cart
-                    </button>
-                </li>
-            ))}
+                        <img src={product.thumbnail} />
+                        <h3>{product.title}</h3>
+                        <span>
+                            <b>PRICE: $</b>
+                            {product.price}
+                        </span>
+                        <button
+                            style={{
+                                width: 'fit-content',
+                                alignSelf: 'center',
+                                margin: '10px',
+                                backgroundColor:productInCart ? 'red' : 'green'
+                            }}
+                            onClick={() => addToCart(product)}
+                        >
+                            {productInCart ? 'added' : 'add to cart'}
+                        </button>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
